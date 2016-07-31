0642fa54ea9e26b4b92ad676e7f628d656ecf924
Colin Dunklau <colin.dunklau@gmail.com>
2016-07-31 15:44:34 -0500

    Remove old lexer, add framework for chunk sequencing

--- pngdoctor/decoder.py
+++ pngdoctor/decoder.py
@@ -1,3 +1,4 @@
+import inspect
 import struct
 import zlib
 from collections import namedtuple
@@ -18,7 +19,7 @@ PNG_SIGNATURE = bytes([
     # Unix line ending (LF)
     0x0A
 ])
-PNG_MAX_FILE_SIZE = 20 * 2**20  # 20 MiB is large enough for most reasonable PNGs
+PNG_MAX_FILE_SIZE = 20 * 2**20  # 20 MiB is large enough for reasonable PNGs
 PNG_MAX_CHUNK_LENGTH = 2**31 - 1  # Max length of chunk data
 PNG_CHUNK_MAX_DATA_READ = 4 * 2**10  # 4 KiB max chunk data processed
 
@@ -300,6 +301,31 @@ class PNGSingleChunkState(object):
 #         zTXt    Yes     None
 
 
+def chunk_handler(chunk_type_code):
+    """
+    Method decorator for chunk handlers.
+
+    Sets an attribute on the function object so the handler dict can
+    be built in :meth:`PNGChunkSequenceValidator.__init__`.
+
+    :param chunk_type_code: The PNG chunk type code.
+    :type chunk_type_code: bytes
+    """
+    if not isinstance(chunk_type_code, bytes):
+        raise TypeError('chunk_type_code must be bytes')
+
+    def decorator(method):
+        method.chunk_type_code = chunk_type_code
+        return method
+
+    return decorator
+
+
+def _ischunkhandler(member):
+    """Predicate for inspect.getmembers"""
+    return inspect.ismethod(member) and hasattr(member, 'chunk_type_code')
+
+
 
 class PNGChunkSequenceValidator(object):
     """
@@ -313,6 +339,13 @@ class PNGChunkSequenceValidator(object):
         self.image_data_seen = False
         # TODO: needs more attributes
 
+        self._handlers = {}
+        for method in inspect.getmembers(self, _ischunkhandler):
+            if method.chunk_type_code in self._handlers:
+                fmt = 'Chunk type code {code} has more than one handler.'
+                raise TypeError(fmt.format(code=method.chunk_type_code))
+            self._handlers[method.chunk_type_code] = method
+
     def __iter__(self):
         """
         Loop over the chunk tokens and validate the head tokens, and
@@ -321,119 +354,6 @@ class PNGChunkSequenceValidator(object):
         for chunk in self.tokens:
             assert False, "this isn't done yet"
 
-
-class PNGLexer(object):
-    def __init__(self, stream):
-        self._stream = stream
-        self._nbytes_read = 0
-
-    def iter_chunks(self):
-        ihdr = None
-        for code, data in self.iter_chunk_type_and_data():
-            chunkcls = models.chunk_registry.get(code, models.UnknownPNGChunk)
-            if ihdr is None:
-                assert chunkcls is models.ImageHeaderPNGChunk
-                chunk = ihdr = chunkcls(data, None)
-            else:
-                chunk = chunkcls(data, ihdr)
-            if chunkcls is models.UnknownPNGChunk:
-                chunk.chunk_type = models.PNGChunkType(code)
-            chunk.parse()
-            yield chunk
-
-    def iter_chunk_type_and_data(self):
-        self._validate_signature()
-
-        chunk = self._get_next_chunk()
-        if chunk[0] != b'IHDR':
-            raise exceptions.PNGSyntaxError("First chunk expected to be IHDR")
-        yield chunk
-
-        while True:
-            chunk = self._get_next_chunk()
-            yield chunk
-            if chunk[0] == b'IEND':
-                break
-
-        # Ensure we are at EOF
-        try:
-            self._read(1)
-        except exceptions.UnexpectedEOF:
-            return
-        else:
-            raise exceptions.PNGSyntaxError(
-                "Data exists beyond IEND chunk end"
-            )
-
-    def _get_next_chunk(self):
-        """
-        Read the next chunk in the file and ensure the CRC matches.
-
-        Return a tuple of (chunk_type, chunk_data), both are bytes.
-        """
-        chunk_start_position = self._nbytes_read + 1
-        length, = struct.unpack('>I', self._read(4))
-        if length > PNG_MAX_CHUNK_LENGTH:
-            fmt = (
-                "Chunk claims to be {actual} bytes long, must be "
-                "no longer than {max}."
-            )
-            raise exceptions.PNGSyntaxError(fmt.format(
-                actual=length,
-                max=PNG_MAX_CHUNK_LENGTH
-            ))
-        chunk_type = self._read(4)
-        chunk_data = self._read(length)
-        expected_crc, = struct.unpack('>I', self._read(4))
-        computed_crc = zlib.crc32(chunk_type)
-        computed_crc = zlib.crc32(chunk_data, computed_crc)
-        if expected_crc != computed_crc:
-            fmt = (
-                "CRC check failed for chunk starting at position {pos}, "
-                "expected {expected:#08x}, computed {computed:#08x}."
-            )
-            raise exceptions.BadCRC(fmt.format(
-                pos=chunk_start_position,
-                expected=expected_crc,
-                computed=computed_crc
-            ))
-        return chunk_type, chunk_data
-
-    def _read(self, length):
-        """
-        Read ``length`` bytes from the stream, update _nbytes_read, and
-        return the bytes.
-
-        If the read results in fewer bytes than requested, raise
-        `UnexpectedEOF`.
-        """
-        if length + self._nbytes_read > PNG_MAX_FILE_SIZE:
-            raise exceptions.PNGTooLarge(
-                "Attempted to read past file size limit: {size} bytes".format(
-                    size=PNG_MAX_FILE_SIZE,
-                )
-            )
-        read = self._stream.read(length)
-        actual = len(read)
-        self._nbytes_read += actual
-        assert length >= actual, "Read more bytes than requested"
-        if length > actual:
-            fmt = "Expected to read {length}, got {actual}, total read {total}"
-            raise exceptions.UnexpectedEOF(
-                fmt.format(
-                    length=length,
-                    actual=actual,
-                    total=self._nbytes_read
-                )
-            )
-        return read
-
-    def _validate_signature(self):
-        header = self._read(len(PNG_SIGNATURE))
-        if header != PNG_SIGNATURE:
-            raise exceptions.SignatureMismatch(
-                "Expected {expected!r}, got {actual!r}".format(
-                    expected=PNG_SIGNATURE,
-                    actual=header
-                )
-            )
+    @chunk_handler(models.IMAGE_HEADER.code)
+    def _handle_image_header(self, token):
+        assert False, "this isn't done yet"
