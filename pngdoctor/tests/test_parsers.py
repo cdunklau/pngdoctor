966f16555c72111c567711d6d01d27b3ff023abc
Colin Dunklau <colin.dunklau@gmail.com>
2016-08-14 00:26:04 +0200

    Be more clear about a few parser tests

--- pngdoctor/tests/test_parsers.py
+++ pngdoctor/tests/test_parsers.py
@@ -21,21 +21,28 @@ class TestChunkOrderParser(object):
     @pytest.mark.parametrize('chunk_codes', [
         [b'IHDR', b'IDAT', b'IEND', b'IDAT'],
         [b'IHDR', b'PLTE', b'IEND'],
-        [b'ukwn'],  # Unknown chunk not allowed first
         # TODO: Add lots more here
     ])
     def test_last_chunk_code_errors(self, chunk_codes, chunk_order_parser):
+        """
+        Ensure that the last provided code provokes an error.
+        """
         from pngdoctor.exceptions import PNGSyntaxError
         for chunk_code in chunk_codes[:-1]:
             chunk_order_parser.validate(chunk_code)
         with pytest.raises(PNGSyntaxError):
             chunk_order_parser.validate(chunk_codes[-1])
 
-    def test_validate_header_must_be_first(self, chunk_order_parser):
+    @pytest.mark.parametrize('non_header_code', [
+        b'PLTE', b'IDAT', b'IEND', b'cHRM', b'gAMA', b'iCCP', b'sBIT', b'sRGB',
+        b'bKGD', b'hIST', b'tRNS', b'pHYs', b'sPLT', b'tIME', b'iTXt', b'tEXt',
+        b'zTXt', b'ukwn',
+    ])
+    def test_validate_only_header_allowed_first(self, non_header_code,
+                                                chunk_order_parser):
         from pngdoctor.exceptions import PNGSyntaxError
-        chunk_code = b'PLTE'
         with pytest.raises(PNGSyntaxError):
-            chunk_order_parser.validate(chunk_code)
+            chunk_order_parser.validate(non_header_code)
 
     def test_validate_end_errors_when_not_finished(self, chunk_order_parser):
         from pngdoctor.exceptions import PNGSyntaxError
