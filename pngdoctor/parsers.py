120c247b9a4865f506fbf15d173c6725c1e5ba97
Colin Dunklau <colin.dunklau@gmail.com>
2016-08-14 18:33:26 +0200

    Move PNG spec notes into separate file

--- pngdoctor/parsers.py
+++ pngdoctor/parsers.py
@@ -9,41 +9,7 @@ from pngdoctor import models
 
 logger = logging.getLogger(__name__)
 
-# From PNG 1.2 specification:
-#
-# This table summarizes some properties of the standard chunk types.
-#
-# Critical chunks (must appear in this order, except PLTE
-#                  is optional):
-#
-#         Name  Multiple  Ordering constraints
-#                 OK?
-#
-#         IHDR    No      Must be first
-#         PLTE    No      Before IDAT
-#         IDAT    Yes     Multiple IDATs must be consecutive
-#         IEND    No      Must be last
-#
-# Ancillary chunks (need not appear in this order):
-#
-#         Name  Multiple  Ordering constraints
-#                 OK?
-#
-#         cHRM    No      Before PLTE and IDAT
-#         gAMA    No      Before PLTE and IDAT
-#         iCCP    No      Before PLTE and IDAT
-#         sBIT    No      Before PLTE and IDAT
-#         sRGB    No      Before PLTE and IDAT
-#         bKGD    No      After PLTE; before IDAT
-#         hIST    No      After PLTE; before IDAT
-#         tRNS    No      After PLTE; before IDAT
-#         pHYs    No      Before IDAT
-#         sPLT    Yes     Before IDAT
-#         tIME    No      None
-#         iTXt    Yes     None
-#         tEXt    Yes     None
-#         zTXt    Yes     None
-
+# ref png-specification-notes.txt
 
 def _code_frozenset(chunk_types):
     """
@@ -288,3 +254,7 @@ class _ChunkOrderStateTransitionMap(collections.abc.Mapping):
 
     def __len__(self):
         return len(self._map)
+
+
+
+# ref png-specification-notes.txt
